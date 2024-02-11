27e3b84d351441e4917c41971f782271e4df4c6d
chessboy71 <bentegrimohamed@gmail.com>
2024-02-11 20:44:37 +0100

    S

--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,38 @@
 #!/usr/bin/python3
-"""
-Module for the BaseModel class.
-"""
+
+""" This file is for the baseModel for the airbnb clone"""
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, time_format))
-                else:
-                    setattr(self, key, value)
-
-        models.storage.new(self)
+        """ this is the contructor of the baseModel class
+        """
+        self.id = uuid.uuid4().__str__()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
+        """ this is the method to save the class
         """
-
-        """
-        self.updated_at = datetime.utcnow()
-        models.storage.save()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
+        """ this is the first function for the serialization
         """
-
-        """
-        inst_dict = self.__dict__.copy()
-        inst_dict["__class__"] = self.__class__.__name__
-        inst_dict["created_at"] = self.created_at.isoformat()
-        inst_dict["updated_at"] = self.updated_at.isoformat()
-
-        return inst_dict
+        mydict = {}
+        for key, value in self.__dict__.items():
+            if type(value) == datetime:
+                mydict[key] = value.isoformat()
+            else:
+                mydict[key] = value
+        mydict["__class__"] = self.__class__.__name__
+        return mydict
 
     def __str__(self):
         """
-
+        str funtion 
         """
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        csname = self.__class__.__name__
+        return "[{}] ({}) {}".format(csname, self.id, self.__dict__)
