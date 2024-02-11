090bf9d7b3aa6dc5dd98f864ee56d6b969733beb
Serour Belabed <68053753+SerourBela@users.noreply.github.com>
2024-02-11 19:15:48 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python3
 """ This file is for the baseModel for the airbnb clone"""
-import uuid
+import models
+from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
     def __init__(self, *args, **kwargs):
         """ this is the contructor of the baseModel class
-        """
-        self.id = uuid.uuid4().__str__()
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+            """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
     def save(self):
         """ this is the method to save the class
         """
         self.updated_at = datetime.now()
-
+        models.storage.save()
     def to_dict(self):
         """ this is the first function for the serialization
         """
